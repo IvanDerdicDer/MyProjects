5f9a59003968e886ef6b328f9b5fa25b87784b8f
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2021-04-30 17:34:17 +0200

    It works

--- firstMissingPositiveInt/firstMissingPositiveInt.py
+++ firstMissingPositiveInt/firstMissingPositiveInt.py
@@ -2,8 +2,9 @@ from bisect import bisect_right
 from random import randint
 from time import thread_time
 
-inputArray = [randint(-1000000, 1000000) for _ in range(10000000)]
+inputArray = [randint(-1000000, 1000000) for _ in range(1000000)]
 #inputArray = [1, 5, -2, 6, -3, 9, 12, -63, 83]
+#inputArray = [1, 1, 1,-4,-5,-1,-8,-12]
 
 def firstMissingPositive(arr: list):
     #This lambda ensures that only positive ints are considered
@@ -11,11 +12,13 @@ def firstMissingPositive(arr: list):
     #By using bisect_right we get the index of the second smallest int
     startIndex = bisect_right(arr, smallestPositiveInt)
     #Move trough the list until the next smallest number is bigger than smallest number + 1
-    while arr[startIndex] == smallestPositiveInt + 1:
+    length = len(arr)
+    while startIndex < length and (arr[startIndex] == smallestPositiveInt + 1):
         #Set the next smallest number
         smallestPositiveInt = arr[startIndex]
         #Find the index of the next smallest number
         startIndex = bisect_right(arr, smallestPositiveInt)
+
 
     return smallestPositiveInt + 1
 
