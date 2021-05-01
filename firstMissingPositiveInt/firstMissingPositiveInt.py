3d2fcdb22c3f1cbc5c669d9d025f26bb3428dcc1
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2021-05-01 16:24:26 +0200

    It works

--- firstMissingPositiveInt/firstMissingPositiveInt.py
+++ firstMissingPositiveInt/firstMissingPositiveInt.py
@@ -7,8 +7,12 @@ inputArray = [randint(-1000000, 1000000) for _ in range(1000000)]
 #inputArray = [1, 1, 1,-4,-5,-1,-8,-12]
 
 def firstMissingPositive(arr: list):
+    #Array needs to be sorted for bisect
+    arr.sort()
+    if 1 not in arr:
+        return 1
     #This lambda ensures that only positive ints are considered
-    smallestPositiveInt = min(arr, key= lambda k: k < 0)
+    smallestPositiveInt = 1
     #By using bisect_right we get the index of the second smallest int
     startIndex = bisect_right(arr, smallestPositiveInt)
     #Move trough the list until the next smallest number is bigger than smallest number + 1
@@ -25,9 +29,12 @@ def firstMissingPositive(arr: list):
 if __name__ == '__main__':
     useArr = inputArray.copy()
     print("Starting...")
-    startTimeMs = thread_time()
-    result = firstMissingPositive(useArr)
-    endTimeMs = thread_time()
-    print(f"Result: {result} Runtime: {endTimeMs - startTimeMs} s")
-    #print(f"InputArray : {inputArray}")
-    #print(f"UsedArray: {useArr}")
+    listOfTimes = []
+    for _ in range(10):
+        useArr = [randint(-1000000, 1000000) for _ in range(1000000)]
+        startTimeMs = thread_time()
+        result = firstMissingPositive(useArr)
+        endTimeMs = thread_time()
+        listOfTimes.append(endTimeMs-startTimeMs)
+
+    print(f"Average time: {sum(listOfTimes)/len(listOfTimes)}")
