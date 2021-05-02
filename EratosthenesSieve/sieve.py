ffd7e493d7c163c7d0ee9571e6e07effe5d706c1
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2021-05-02 11:44:22 +0200

    Radi bolje

--- EratosthenesSieve/sieve.py
+++ EratosthenesSieve/sieve.py
@@ -1,4 +1,5 @@
 from time import  thread_time
+from math import sqrt
 
 def isPrime(n):
     if n == 1:
@@ -25,24 +26,24 @@ def isPrime2(n: int, arr: list):
             return False
     return True
 
-def eratosthenesSieve(arr: list):
+def eratosthenesSieve(arr: list, n):
     i = 0
-    primes = []
-    length = len(arr)
+    length = sqrt(n)
     while i < length:
-        if isPrime2(arr[i], primes):
-            primes.append(arr[i])
+        for j in arr:
+            if j%arr[i] == 0 and j != arr[i]:
+                arr.remove(j)
 
         i += 1
-    return primes
+    return arr
 
 if __name__ == '__main__':
-    n = 1000
+    n = 10000
     listOfNumbers = [2]+[i for i in range(3, n, 2)]
     listToUse = listOfNumbers.copy()
     numberOfPrimes = 1
 
-    primes = eratosthenesSieve(listToUse)
+    primes = eratosthenesSieve(listToUse, n)
 
     print(f"ListOfNumbers: {primes}\nlen: {len(primes)}\nallPrimes: {allPrimes(primes)}")
 
@@ -50,7 +51,7 @@ if __name__ == '__main__':
     numberOfPasses = 0
     endTime = thread_time()
     while endTime - startTime < 5:
-        eratosthenesSieve(listToUse)
+        eratosthenesSieve(listToUse, n)
         numberOfPasses += 1
         endTime = thread_time()
 
