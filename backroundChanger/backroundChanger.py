a09482aadf1ec1783a35526e5abb376ec1ada844
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2021-05-29 15:54:30 +0200

    Made the code more GUI friendly

--- backroundChanger/backroundChanger.py
+++ backroundChanger/backroundChanger.py
@@ -2,6 +2,7 @@ import ctypes
 import os
 from datetime import timedelta
 from time import time, sleep
+from threading import Thread
 
 
 def splitDayIntoParts(n: int) -> list[timedelta]:
@@ -26,24 +27,38 @@ def chooseWallpaper(dayIntervals: list[timedelta], currentTime:timedelta) -> int
         if currentTime < intervalStart:
             return dayIntervals.index(intervalStart)
 
-if __name__ == '__main__':
-    #Absolute path is necessary for the wallpaper path
-    pathToWallpapers = os.path.abspath("wallpapers")
+def initialiseRelevantVariables(relativePath: str):
+    #Converts relative path to absolute path
+    pathToWallpapers = os.path.abspath(relativePath)
     wallpapersIterator = os.scandir(pathToWallpapers)
-
+    #List of absolute paths for each wallpaper
     pathToWallpaper = [i.path for i in wallpapersIterator]
     dayIntervals = splitDayIntoParts(len(pathToWallpaper))
 
+    return pathToWallpaper, dayIntervals
+
+def wallpaperChangingLoop(pathToWallpaper: list[str], dayIntervals: list[timedelta]):
     previousIndex = 0
     while True:
         print("Entered loop")
         currentTime = getCurrentTime()
         currentIndex = chooseWallpaper(dayIntervals, currentTime)
         print(f"currentTime: {currentTime}, currentIndex: {currentIndex}, previousIndex: {previousIndex}")
-        #Makes sure wallpaper isn't changed constantly
+        # Makes sure wallpaper isn't changed constantly
         if currentIndex != previousIndex:
             changeWallpaper(pathToWallpaper[currentIndex])
             print("Wallpaper changed")
             previousIndex = currentIndex
         sleep(30)
 
+if __name__ == '__main__':
+    #Relative path to wallpapers folder
+    #In the future should be changed trough a GUI to select wallpaper batch
+    relativePath = "wallpapers"
+
+    pathToWallpaper, dayIntervals = initialiseRelevantVariables()
+
+    #Should be run in a new thread when GUI is implemented
+    #wallpaperLoopThread = Thread(target=wallpaperChangingLoop, args=(pathToWallpaper, dayIntervals))
+    #wallpaperLoopThread.start()
+    wallpaperChangingLoop(pathToWallpaper, dayIntervals)
